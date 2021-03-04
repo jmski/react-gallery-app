657f73f7330e4a8a8a8fb5aab53590fbc168cf0f
= <geluajm@gmail.com>
2021-03-03 21:39:40 -0500

    added project files and organized folder

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+	BrowserRouter,
+	Route,Switch
+} from 'react-router-dom';
+
+const App = () => (
+	<BrowserRouter basename="react-gallery-app">
+		<div className="container">
+		
+		</div>
+		
+	</BrowserRouter>
+);
+
+
+export default App;
